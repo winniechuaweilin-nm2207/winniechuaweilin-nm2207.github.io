e7caeb8cdb259d391e239efa3ebeb6e5428ea486
winniechuaweilin-nm2207 <125366338+winniechuaweilin-nm2207@users.noreply.github.com>
2023-03-17 18:17:22 +0800

    17MarEdit

--- scripts/game.js
+++ scripts/game.js
@@ -2,6 +2,10 @@ function selectCell() {
     chooseCell(this);
 }
 
+function rightClickCell() {
+    plantFlag(this);
+}
+
 //set up playing field
 window.onload = function showTable() {
     document.getElementById("table").innerHTML="";
@@ -9,18 +13,20 @@ window.onload = function showTable() {
         row = table.insertRow(r);
         for(let c=0; c<20; c++) {
             cell = row.insertCell(c);
+            cell.className="black";
             //function activated when a cell is clicked
             cell.addEventListener("click", selectCell);
-            // //creating bomb attribute
-            // let bomb = document.createAttribute("bomb");
-            // //set value of bomb attribute
-            // bomb.value="safe";
+            //function activated when a cell is right-clicked
+            cell.addEventListener('contextmenu', rightClickCell);
             //add bomb attribute to cell
             cell.setAttribute("bomb","safe");
-            console.log(cell.getAttribute("bomb"));
+            // console.log(cell.getAttribute("bomb"));
+            //add open attribute to cell
+            cell.setAttribute("open","false");
         }
     }
     insertBombs();
+    startTimer();
 }
 
 //allocate bombs to cells randomly
@@ -29,10 +35,10 @@ function insertBombs() {
         var row = Math.floor(Math.random() * 15);
         var col = Math.floor(Math.random() * 20);
         var cell = table.rows[row].cells[col];
-        console.log(cell);
+        // console.log(cell);
             if(cell.getAttribute("bomb")==="safe") {
                 cell.setAttribute("bomb","explode");
-                cell.innerHTML="X";
+                //cell.innerHTML="X";
             } else {
                 --i;
             }
@@ -51,41 +57,86 @@ function showBombLocation() {
     }
 }
 
+let countflag = 0;
+let flagCorrect = 0;
+//right click to demark potential "bombs"
+function plantFlag(select) {
+    if (select.className === "black" && countflag < 50){
+        select.className = "suspectbomb";
+        select.setAttribute("open", "true");
+        countflag += 1;
+        document.getElementById("count").innerHTML = "Bomb Count: " + countflag;
+        //count correct bomb location
+        if(select.getAttribute("bomb") === "explode") {
+            flagCorrect += 1;
+            console.log(flagCorrect);
+        }
+        //check if player won
+        if(flagCorrect === 50) {
+            setTimeout(function(){
+                window.alert("Congrats " + localStorage.getItem("saveName") + ", you saved the Universe in " + time + "s! Proud of you buddy!");
+            },10);
+            stopTimer();
+            for (let r=0; r<15; r++) {
+                for (let c=0; c<20; c++) {
+                    let cell = table.rows[r].cells[c];
+                    cell.removeEventListener("click", selectCell);
+                }
+            }
+        }
+    } 
+    else if (select.className === "suspectbomb" && flagCorrect !== 50) {
+        select.className = "black";
+        select.setAttribute("open", "false");
+        countflag -= 1;
+        document.getElementById("count").innerHTML = "Bomb Count: " + countflag;
+        if(select.getAttribute("bomb") === "explode") {
+            flagCorrect -= 1;
+            console.log(flagCorrect);
+        }
+    }  
+}
 
 //when cell is clicked
 function chooseCell(select) {
+    //if cell has been clicked then dont click again
+    if (select.getAttribute("open") === "true"){
+        return;
+    } else {
+        select.setAttribute("open","true");
+    }
     select.className="changecolor";
         let r = select.parentNode.rowIndex;
         let c = select.cellIndex;
-        console.log(r);
-        console.log(c); 
+        // console.log(r);
+        // console.log(c);        
     if (select.getAttribute("bomb") === "safe") {
         //show number of bombs around the cell
         if (r-1 >= 0 && r+1 < 15 && c-1 >= 0 && c+1 < 20) {
             var count = 0
             //top
-            if(table.rows[r-1].cells[c].innerHTML === "X") {
+            if(table.rows[r-1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom
-            if(table.rows[r+1].cells[c].innerHTML === "X") {
+            if(table.rows[r+1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //left
-            if(table.rows[r].cells[c-1].innerHTML === "X") {
+            if(table.rows[r].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //right
-            if(table.rows[r].cells[c+1].innerHTML === "X") {
+            if(table.rows[r].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom left
-            if(table.rows[r+1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom right
-            if(table.rows[r+1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top left
-            if(table.rows[r-1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top right
-            if(table.rows[r-1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -93,19 +144,19 @@ function chooseCell(select) {
         if (r-1 >= 0 && r+1 < 15 && c-1 == -1 && c+1 < 20) {
             var count = 0
             //top
-            if(table.rows[r-1].cells[c].innerHTML === "X") {
+            if(table.rows[r-1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom
-            if(table.rows[r+1].cells[c].innerHTML === "X") {
+            if(table.rows[r+1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //right
-            if(table.rows[r].cells[c+1].innerHTML === "X") {
+            if(table.rows[r].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom right
-            if(table.rows[r+1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top right
-            if(table.rows[r-1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -113,19 +164,19 @@ function chooseCell(select) {
         if (r-1 >= 0 && r+1 < 15 && c-1 >= 0 && c+1 == 20) {
             var count = 0
             //top
-            if(table.rows[r-1].cells[c].innerHTML === "X") {
+            if(table.rows[r-1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom
-            if(table.rows[r+1].cells[c].innerHTML === "X") {
+            if(table.rows[r+1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //left
-            if(table.rows[r].cells[c-1].innerHTML === "X") {
+            if(table.rows[r].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom left
-            if(table.rows[r+1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top left
-            if(table.rows[r-1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -133,19 +184,19 @@ function chooseCell(select) {
         if (r-1 == -1 && r+1 < 15 && c-1 >= 0 && c+1 < 20) {
             var count = 0
             //bottom
-            if(table.rows[r+1].cells[c].innerHTML === "X") {
+            if(table.rows[r+1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //left
-            if(table.rows[r].cells[c-1].innerHTML === "X") {
+            if(table.rows[r].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //right
-            if(table.rows[r].cells[c+1].innerHTML === "X") {
+            if(table.rows[r].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom left
-            if(table.rows[r+1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom right
-            if(table.rows[r+1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -153,19 +204,19 @@ function chooseCell(select) {
         if (r-1 >= 0 && r+1 == 15 && c-1 >= 0 && c+1 < 20) {
             var count = 0
             //top
-            if(table.rows[r-1].cells[c].innerHTML === "X") {
+            if(table.rows[r-1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //left
-            if(table.rows[r].cells[c-1].innerHTML === "X") {
+            if(table.rows[r].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //right
-            if(table.rows[r].cells[c+1].innerHTML === "X") {
+            if(table.rows[r].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top left
-            if(table.rows[r-1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top right
-            if(table.rows[r-1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -173,13 +224,13 @@ function chooseCell(select) {
         if (r-1 == -1 && c-1 == -1) {
             var count = 0
             //bottom
-            if(table.rows[r+1].cells[c].innerHTML === "X") {
+            if(table.rows[r+1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //right
-            if(table.rows[r].cells[c+1].innerHTML === "X") {
+            if(table.rows[r].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom right
-            if(table.rows[r+1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -187,13 +238,13 @@ function chooseCell(select) {
         if (r-1 == -1 && c+1 == 20) {
             var count = 0
             //bottom
-            if(table.rows[r+1].cells[c].innerHTML === "X") {
+            if(table.rows[r+1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //left
-            if(table.rows[r].cells[c-1].innerHTML === "X") {
+            if(table.rows[r].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //bottom left
-            if(table.rows[r+1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r+1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -201,13 +252,13 @@ function chooseCell(select) {
         if (r+1 == 15 && c-1 == -1) {
             var count = 0
             //top
-            if(table.rows[r-1].cells[c].innerHTML === "X") {
+            if(table.rows[r-1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //right
-            if(table.rows[r].cells[c+1].innerHTML === "X") {
+            if(table.rows[r].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top right
-            if(table.rows[r-1].cells[c+1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c+1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -215,13 +266,13 @@ function chooseCell(select) {
         if (r+1 == 15 && c+1 == 20) {
             var count = 0
             //top
-            if(table.rows[r-1].cells[c].innerHTML === "X") {
+            if(table.rows[r-1].cells[c].getAttribute("bomb") === "explode") {
                 count += 1;
             } //left
-            if(table.rows[r].cells[c-1].innerHTML === "X") {
+            if(table.rows[r].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             } //top left
-            if(table.rows[r-1].cells[c-1].innerHTML === "X") {
+            if(table.rows[r-1].cells[c-1].getAttribute("bomb") === "explode") {
                 count += 1;
             }
             table.rows[r].cells[c].innerHTML = count;
@@ -230,7 +281,8 @@ function chooseCell(select) {
         showBombLocation();
         setTimeout(function (){
              window.alert("CHIKABOOM! Sorry " + localStorage.getItem("saveName") + ", you lost. Try harder next time!");
-        },10)
+        },10);
+        stopTimer();
         for (let r=0; r<15; r++) {
             for (let c=0; c<20; c++) {
                 let cell = table.rows[r].cells[c];
@@ -238,11 +290,122 @@ function chooseCell(select) {
             }
         }
     }
-    reveal()
-}
-
-function reveal() {
-    if (table.rows[r].cells[c].innerHTML === 0) {
-        //reveal cell till there is no 0
+    if (table.rows[r].cells[c].innerHTML == 0) {
+        //reveal bombs numbers around cell that has 0 bomb
+        if (r-1 >= 0 && r+1 < 15 && c-1 >= 0 && c+1 < 20){
+            //top
+            chooseCell(table.rows[r-1].cells[c]);
+            //bottom
+            chooseCell(table.rows[r+1].cells[c]);
+            //left
+            chooseCell(table.rows[r].cells[c-1]);
+            //right
+            chooseCell(table.rows[r].cells[c+1]);
+            //top right
+            chooseCell(table.rows[r-1].cells[c+1]);
+            //top left
+            chooseCell(table.rows[r-1].cells[c-1]);
+            //bottom right
+            chooseCell(table.rows[r+1].cells[c+1]);
+            //bottom left
+            chooseCell(table.rows[r+1].cells[c-1]);
+        } //left column of playing field 
+        if ((r-1 >= 0 && r+1 < 15 && c-1 == -1 && c+1 < 20)) {
+            //top
+            chooseCell(table.rows[r-1].cells[c]);
+            //bottom
+            chooseCell(table.rows[r+1].cells[c]);
+            //right
+            chooseCell(table.rows[r].cells[c+1]);
+            //top right
+            chooseCell(table.rows[r-1].cells[c+1]);
+            //bottom right
+            chooseCell(table.rows[r+1].cells[c+1]);
+        } //code for right column of playing field
+        if (r-1 >= 0 && r+1 < 15 && c-1 >= 0 && c+1 == 20) {
+            //top
+            chooseCell(table.rows[r-1].cells[c]);
+            //bottom
+            chooseCell(table.rows[r+1].cells[c]);
+            //left
+            chooseCell(table.rows[r].cells[c-1]);
+            //top left
+            chooseCell(table.rows[r-1].cells[c-1]);
+            //bottom left
+            chooseCell(table.rows[r+1].cells[c-1]);
+        } //code for top row of playing field
+        if (r-1 == -1 && r+1 < 15 && c-1 >= 0 && c+1 < 20) {
+            //bottom
+            chooseCell(table.rows[r+1].cells[c]);
+            //left
+            chooseCell(table.rows[r].cells[c-1]);
+            //right
+            chooseCell(table.rows[r].cells[c+1]);
+            //bottom right
+            chooseCell(table.rows[r+1].cells[c+1]);
+            //bottom left
+            chooseCell(table.rows[r+1].cells[c-1]);
+        } //code for bottom row of playing field
+        if (r-1 >= 0 && r+1 == 15 && c-1 >= 0 && c+1 < 20) {
+            //top
+            chooseCell(table.rows[r-1].cells[c]);
+            //left
+            chooseCell(table.rows[r].cells[c-1]);
+            //right
+            chooseCell(table.rows[r].cells[c+1]);
+            //top right
+            chooseCell(table.rows[r-1].cells[c+1]);
+            //top left
+            chooseCell(table.rows[r-1].cells[c-1]);
+        } //hardcode for top left of playing field
+        if (r-1 == -1 && c-1 == -1) {
+            //bottom
+            chooseCell(table.rows[r+1].cells[c]);
+            //right
+            chooseCell(table.rows[r].cells[c+1]);
+            //bottom right
+            chooseCell(table.rows[r+1].cells[c+1]);
+        } //hardcode for top right of playing field
+        if (r-1 == -1 && c+1 == 20) {
+            //bottom
+            chooseCell(table.rows[r+1].cells[c]);
+            //left
+            chooseCell(table.rows[r].cells[c-1]);
+            //bottom left
+            chooseCell(table.rows[r+1].cells[c-1]);
+        } //hardcode for bottom left of playing field
+        if (r+1 == 15 && c-1 == -1) {
+            //top
+            chooseCell(table.rows[r-1].cells[c]);
+            //right
+            chooseCell(table.rows[r].cells[c+1]);
+            //top right
+            chooseCell(table.rows[r-1].cells[c+1]);
+        } //hardcode for bottom right of playing field
+        if (r+1 == 15 && c+1 == 20) {
+            //top
+            chooseCell(table.rows[r-1].cells[c]);
+            //left
+            chooseCell(table.rows[r].cells[c-1]);
+            //top left
+            chooseCell(table.rows[r-1].cells[c-1]);
+        }
     }
 }
+
+//Set timer
+var time = 0;
+
+function startTimer() {
+    time += 001;
+    document.getElementById("timer").innerHTML= "Timer: "+ time + "s";
+}
+var Countup = setInterval(startTimer,1000);
+
+//stop timer
+function stopTimer() {
+    clearInterval(Countup);
+    return time;
+}
+
+
